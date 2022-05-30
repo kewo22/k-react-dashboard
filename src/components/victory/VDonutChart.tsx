15896b01e28fbaf613ad2c9a2fdf84fb38956a52
kewin-22 <kewinf271@gmail.com>
2022-05-30 15:33:10 +0530

    fix

--- src/components/victory/VDonutChart.tsx
+++ src/components/victory/VDonutChart.tsx
@@ -77,7 +77,6 @@ export default function VDonutChart() {
                     fill="#000000"
                     x="185"
                     y="200"
-                    fontFamily='monospace'
                     fontSize={24}
                     fontWeight="bold"
                 >
@@ -87,7 +86,6 @@ export default function VDonutChart() {
                     fill="#000000"
                     x="145"
                     y="218"
-                    fontFamily='monospace'
                     fontSize={16}
                 >
                     Total Comments
