795f311c898fa459099d199eb07914dee531310d
kewin-22 <kewinf271@gmail.com>
2022-06-02 11:48:04 +0530

    curve fix

--- src/components/react-vis-components/ReactVisLineChart.tsx
+++ src/components/react-vis-components/ReactVisLineChart.tsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalGridLines, LabelSeries, VerticalBarSeriesCanvas, VerticalBarSeries, HorizontalBarSeries, RadialChart, Borders, Hint } from 'react-vis';
-import { Line } from 'victory';
 import styles from '../../../styles/Home.module.css'
+import { curveCatmullRom } from 'd3-shape';
 
 export default function ReactVisLine() {
 
-    const data = [{ x: 1, y: 3 }, { x: 2, y: 5 }, { x: 3, y: 15 }, { x: 4, y: 12 }]
+    const data = [
+        { x: 1, y: 3 }, 
+        { x: 2, y: 5 }, 
+        { x: 3, y: 15 }, 
+        { x: 4, y: 12 }
+    ]
 
     return (
 
         <XYPlot width={300} height={300}>
 
-            <Line
-                data={[{ x: 1, y: 3 }, { x: 2, y: 5 }, { x: 3, y: 15 }, { x: 4, y: 12 }]}
+            <LineSeries
+                data={data}
+                curve={curveCatmullRom.alpha(0.5)}
+                style={{ fill: 'none', strokeLinejoin: "round" }}
             />
+
             <HorizontalGridLines />
             <VerticalGridLines />
             <XAxis />
